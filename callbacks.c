0d3a2ce719372afa73d446e95c0dd04b41b92c16
Raziq Raif <42981908+raziqraif@users.noreply.github.com>
2019-11-24 00:47:34 -0500

    Insert into trv_processes & add callback files.

---
+++ callbacks.c
@@ -0,0 +1,9 @@
+#include "callbacks.h"
+
+/*
+ * Called when application is destroyed
+ */
+
+void on_application_destroy() {
+
+} /* on_application_destroy() */
---
+++ callbacks.h
@@ -0,0 +1,6 @@
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+
+void on_application_destroy();
+
+#endif //CALLBACKS_H
